41e369f6edf6bb47e6e10a915466716a07956247
DESKTOP-LPME90A\a0970 <a0970366492@gmail.com>
2018-11-02 08:14:17 +0800

    no message

--- MyAndroidTutorial/app/src/main/java/net/macdidi/myandroidtutorial/ItemActivity.java
+++ MyAndroidTutorial/app/src/main/java/net/macdidi/myandroidtutorial/ItemActivity.java
@@ -1,11 +1,6 @@
 package net.macdidi.myandroidtutorial;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.MediaStore;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
